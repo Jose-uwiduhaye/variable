4f999ae47ae2537e1c0581b4ad87273ccc500044
uwiduhaye1 <joseuwiduhayee@gmail.com>
2023-11-28 01:12:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+ 
+
+let name0 = "Maxwell Wright";
+let phone0 = "(0191) 719 6495";
+let email0 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let name1 = "Raja Villarreal";
+let phone1 = "0866 398 2895";
+let email1 = "posuere.vulputate@sed.com";
+
+
+let name2 = "Helen Richards";
+let phone2 = "0800 1111";
+let email2 = "libero@convallis.edu";
+
+
+console.log("First Contact:");
+console.log("Name: " + name0);
+console.log("Phone: " + phone0);
+console.log("Email: " + email0);
+
+console.log("\nLast Contact:");
+console.log("Name: " + name2);
+console.log("Phone: " + phone2);
+console.log("Email: " + email2);
+
+
